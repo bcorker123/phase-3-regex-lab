ec89d1f02aaa9a60ba9f1b626f62f30dcd294c50
brian corker <bcorker123@gmail.com>
2022-06-08 16:40:37 -0400

    commit tree 66305e338a4938c9655049b56f72ad1fbba7f51e

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+    #puts "word: #{word}, return: #{word.start_with?(/[aeiouAEIOU]/)}"
+    word.start_with?(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    puts "text array: #{text.split}, return: #{}"
+    text.split.filter do |word|
+        pattern = "/un.#{word.length-5}ing/"
+        word.scan(pattern)
+    end
 end
 
 def words_five_letters_long(text)
